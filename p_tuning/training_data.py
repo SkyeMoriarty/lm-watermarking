f623970423f63712f4bb9e34859cfcf896d1f2f3
SkyeMoriarty <2450996872@qq.com>
2025-07-18 11:24:29 +0100

    edit load_data

--- p_tuning/training_data.py
+++ p_tuning/training_data.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 
 
 def load_data():
-    dataset = load_dataset("ag_news", split="train", trust_remote_code=True)
+    dataset = load_dataset("dbpedia_14", split="train")
     return dataset
 
 
