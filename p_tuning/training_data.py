9e18b5f4e2ca12924adec58870a334708367293b
SkyeMoriarty <2450996872@qq.com>
2025-07-18 14:52:46 +0100

    save to json

--- p_tuning/training_data.py
+++ p_tuning/training_data.py
@@ -12,6 +12,8 @@ from watermark import generate
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
+import json
+
 
 def load_data():
     dataset = load_dataset("ag_news", split="train")
@@ -19,10 +21,17 @@ def load_data():
     return subset
 
 
-def get_dataloader(dataset, batch_size=2):
+def get_dataloader(dataset, batch_size=4):
     # collate_fn参数指定如何将一批样本打包成一个batch
     dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=lambda x: [e["text"] for e in x])
     return dataloader
+
+
+def save_to_json(inputs, targets):
+    with open("./p_tuning_data.jsonl", "w") as f:
+        for input, target in zip(inputs, targets):
+            json.dump({"input": input, "target": target}, f)
+            f.write("\n")
 
 
 def get_train_data(args):
@@ -36,25 +45,25 @@ def get_train_data(args):
     else:
         model, tokenizer, device = None, None, None
 
-    train_data = []
+    inputs = []  # 存储list[str]的列表
+    targets = []
     if not args.skip_model_load:
         dataset = load_data()
         dataloader = get_dataloader(dataset)
         for prompts in dataloader:  # 遍历batch
             redecoded_input, _, decoded_output_with_watermark, _ = generate(prompts, args, model=model,
                                                                             device=device, tokenizer=tokenizer)
+            for input_str, target_str in zip(redecoded_input, decoded_output_with_watermark):
+                inputs.append(input_str)
+                targets.append(target_str)
             print()
             print("Input: ")
             print(redecoded_input)
             print()
             print("Target: ")
             print(decoded_output_with_watermark)
-            train_data.append(decoded_output_with_watermark)
-        print("len: " + str(len(train_data)))
-    return train_data
 
-
-# def format_train_data(train_data):
+    save_to_json(inputs, targets)
 
 
 if __name__ == "__main__":
