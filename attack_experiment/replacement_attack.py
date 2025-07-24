6f35e2ad87eafdadd014ecaef4dc7fe9663e415f
SkyeMoriarty <2450996872@qq.com>
2025-07-24 12:34:44 +0100

    debug

--- attack_experiment/replacement_attack.py
+++ attack_experiment/replacement_attack.py
@@ -1,5 +1,7 @@
 from demo_watermark import parse_args, load_model, generate, detect
 from attack_models.replacement import replacement_attack
+from datasets import load_dataset
+import json
 
 input_text = (
     "The diamondback terrapin or simply terrapin (Malaclemys terrapin) is a "
@@ -24,11 +26,18 @@ input_text = (
     "feet.[9] The species is"
 )
 
+dataset = load_dataset("c4", "en", split="train[:500]")
+epsilons = [0.1, 0.3, 0.5, 0.9]
 
-def main(args):
-    # 解析参数
-    args = parse_args(args)
 
+def save_to_json(output):
+    with open("./result.jsonl", "a") as f:
+        for k, v in output.items():
+            json.dump({k: v}, f)
+            f.write("\n")
+
+
+def main(args, input=input_text, epsilon=0.1):
     args.normalizers = (args.normalizers.split(",") if args.normalizers else [])
     print(args)
 
@@ -38,25 +47,46 @@ def main(args):
     else:
         model, tokenizer, device, _ = None, None, None, None
 
+    output_dict = {}
+
+    if args.use_sampling:
+        output_dict["sampling"] = "m-nom"
+    else:
+        output_dict["sampling"] = "8-beams"
+    output_dict["z threshold"] = args.detection_z_threshold
+
     # 截取prompt，得到有/无水印的生成文本
-    prompt, _, output_without_watermark, output_with_watermark, _ = generate(input_text, args, model=model,
+    prompt, _, output_without_watermark, output_with_watermark, _ = generate(input, args, model=model,
                                                                              device=device, tokenizer=tokenizer)
     print("prompt: ", prompt)
+    output_dict["prompt"] = prompt
 
     # 攻击水印文本
     attacked_output = replacement_attack(output_with_watermark)
 
     # 分别检测有/无受攻击的水印文本
     original_result, _ = detect(output_with_watermark, args, device=device, tokenizer=tokenizer)
-    print("original watermarked output: " + output_with_watermark)
+    print("original watermarked completion: " + output_with_watermark)
     print("green fraction: " + original_result['Fraction of T in Greenlist'])
     print("z score: " + original_result['z-score'])
     print("prediction: " + original_result['Prediction'])
+    print()
+    output_dict["original watermarked completion"] = output_with_watermark
+    output_dict["original green fraction"] = original_result['Fraction of T in Greenlist']
+    output_dict["original z score"] = original_result['z-score']
+    output_dict["original prediction"] = original_result['Prediction']
 
     attacked_result, _ = detect(attacked_output, args, device=device, tokenizer=tokenizer)
-    print("attacked watermarked output: " + attacked_output)
+    print("attacked watermarked completion: " + attacked_output)
     print("green fraction: " + attacked_result['Fraction of T in Greenlist'])
     print("z score: " + attacked_result['z-score'])
     print("prediction: " + attacked_result['Prediction'])
+    output_dict["attacked watermarked completion"] = attacked_output
+    output_dict["attacked green fraction"] = attacked_result['Fraction of T in Greenlist']
+    output_dict["attacked z score"] = attacked_result['z-score']
+    output_dict["attacked prediction"] = attacked_result['Prediction']
 
+    save_to_json(output_dict)
+
+    return output_dict
 
