8b0646fa521a33d3fd8804cb48409b3b2e3de814
SkyeMoriarty <2450996872@qq.com>
2025-07-24 12:35:33 +0100

    debug

--- attack_experiment/replacement_attack.py
+++ attack_experiment/replacement_attack.py
@@ -26,7 +26,7 @@ input_text = (
     "feet.[9] The species is"
 )
 
-dataset = load_dataset("c4", "en", split="train[:500]")
+# dataset = load_dataset("c4", "en", split="train[:500]")
 epsilons = [0.1, 0.3, 0.5, 0.9]
 
 
