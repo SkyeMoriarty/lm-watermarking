d904d2ed42ba59f2f627bc6aa32cdd27dfbd5965
SkyeMoriarty <2450996872@qq.com>
2025-07-24 13:06:51 +0100

    debug

--- attack_experiment/replacement_attack.py
+++ attack_experiment/replacement_attack.py
@@ -1,7 +1,9 @@
+import csv
+import os
+
 from demo_watermark import parse_args, load_model, generate, detect
 from attack_models.replacement import replacement_attack
 from datasets import load_dataset
-import json
 
 input_text = (
     "The diamondback terrapin or simply terrapin (Malaclemys terrapin) is a "
@@ -26,30 +28,38 @@ input_text = (
     "feet.[9] The species is"
 )
 
-# dataset = load_dataset("c4", "en", split="train[:500]")
+dataset = load_dataset("c4", "en", split="train[:3]")
 epsilons = [0.1, 0.3, 0.5, 0.9]
 
 fieldnames = [
     "sampling",
     "epsilon",
-
+    "z threshold",
+    "prompt",
     "original watermarked completion",
     "original green fraction",
     "original z score",
-    "original prediction"
+    "original prediction",
+    "attacked watermarked completion",
+    "attacked green fraction",
+    "attacked z score",
+    "attacked prediction",
 ]
+output_path = "./result.csv"
+if not os.path.exists(output_path):
+    with open(output_path, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()  # 写入表头
 
 
-def save_to_json(output):
-    with open("./result.jsonl", "a") as f:
-        for k, v in output.items():
-            json.dump({k: v}, f)
-            f.write("\n")
+def save_to_csv(output_dicts):
+    with open(output_path, mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerows(output_dicts)
 
 
-def main(args, input=input_text, epsilon=0.1):
+def get_single_output_dict(args, input=input_text, epsilon=0.1):
     args.normalizers = (args.normalizers.split(",") if args.normalizers else [])
-    print(args)
 
     # 加载模型、分词器、device
     if not args.skip_model_load:
@@ -63,12 +73,12 @@ def main(args, input=input_text, epsilon=0.1):
         output_dict["sampling"] = "m-nom"
     else:
         output_dict["sampling"] = "8-beams"
+    output_dict["epsilon"] = epsilon
     output_dict["z threshold"] = args.detection_z_threshold
 
     # 截取prompt，得到有/无水印的生成文本
     prompt, _, output_without_watermark, output_with_watermark, _ = generate(input, args, model=model,
                                                                              device=device, tokenizer=tokenizer)
-    print("prompt: ", prompt)
     output_dict["prompt"] = prompt
 
     # 攻击水印文本
@@ -77,11 +87,11 @@ def main(args, input=input_text, epsilon=0.1):
     # 分别检测有/无受攻击的水印文本
     original_result, _ = detect(output_with_watermark, args, device=device, tokenizer=tokenizer)
     original_result = dict(original_result)
-    print("original watermarked completion: " + output_with_watermark)
-    print("green fraction: " + original_result['Fraction of T in Greenlist'])
-    print("z score: " + original_result['z-score'])
-    print("prediction: " + original_result['Prediction'])
-    print()
+    # print("original watermarked completion: " + output_with_watermark)
+    # print("green fraction: " + original_result['Fraction of T in Greenlist'])
+    # print("z score: " + original_result['z-score'])
+    # print("prediction: " + original_result['Prediction'])
+    # print()
     output_dict["original watermarked completion"] = output_with_watermark
     output_dict["original green fraction"] = original_result['Fraction of T in Greenlist']
     output_dict["original z score"] = original_result['z-score']
@@ -89,17 +99,25 @@ def main(args, input=input_text, epsilon=0.1):
 
     attacked_result, _ = detect(attacked_output, args, device=device, tokenizer=tokenizer)
     attacked_result = dict(attacked_result)
-    print("attacked watermarked completion: " + attacked_output)
-    print("green fraction: " + attacked_result['Fraction of T in Greenlist'])
-    print("z score: " + attacked_result['z-score'])
-    print("prediction: " + attacked_result['Prediction'])
+    # print("attacked watermarked completion: " + attacked_output)
+    # print("green fraction: " + attacked_result['Fraction of T in Greenlist'])
+    # print("z score: " + attacked_result['z-score'])
+    # print("prediction: " + attacked_result['Prediction'])
     output_dict["attacked watermarked completion"] = attacked_output
     output_dict["attacked green fraction"] = attacked_result['Fraction of T in Greenlist']
     output_dict["attacked z score"] = attacked_result['z-score']
     output_dict["attacked prediction"] = attacked_result['Prediction']
 
-    # 不断更新，最后统一写入
-    save_to_json(output_dict)
-
     return output_dict
 
+
+def get_output_dicts(args):
+    output_dicts = {}
+
+    for item in dataset:
+        text = item["text"]
+        for epsilon in epsilons:
+            output_dicts.update(get_single_output_dict(args, text, epsilon))
+
+    save_to_csv(output_dicts)
+    return output_dicts
