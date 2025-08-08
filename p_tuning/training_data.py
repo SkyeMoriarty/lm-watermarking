b5e6a43b363c02d3573aa484512a66282312e2fc
SkyeMoriarty <2450996872@qq.com>
2025-08-08 20:52:04 +0100

    debug

--- p_tuning/training_data.py
+++ p_tuning/training_data.py
@@ -16,7 +16,7 @@ import json
 
 def load_data():
     dataset = load_dataset("ag_news", split="train")
-    subset = dataset.select(range(500))
+    subset = dataset.select(range(500, 1000))
     return subset
 
 
